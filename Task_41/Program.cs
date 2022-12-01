c43bfbc2c2501d2d83771ec7840681a9251e0564
Maxim <ilyukhin.m@internet.ru>
2022-12-01 21:07:55 +0300

    first commit

---
+++ Task_41/Program.cs
@@ -0,0 +1,27 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+using static System.Console;
+Clear();
+int[] GetintFromConsole(string message)
+{
+    Write(message);
+    return Array.ConvertAll(ReadLine()!.Split(), int.Parse);
+}
+
+void CountPositiveElements(int[] Arr)
+{
+    int count = 0;
+    for (int i = 0; i < Arr.Length; i++)
+    {
+        if (Arr[i] > 0)
+            count++;
+    }
+    WriteLine($"Number of positive elements = {count}");
+}
+
+int[] message = GetintFromConsole("Enter elements(separated by space): ");
+CountPositiveElements(message);
+
---
+++ Task_43/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+using static System.Console;
+Clear();
+
+double GetIntFromConsole(string message)
+{
+    Write(message);
+    return double.Parse(ReadLine()!);
+}
+
+void IntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    WriteLine($"({x}; {y})");
+}
+
+double message1 = GetIntFromConsole("Enter b1 = ");
+double message2 = GetIntFromConsole("Enter k1 = ");
+double message3 = GetIntFromConsole("Enter b2 = ");
+double message4 = GetIntFromConsole("Enter k2 = ");
+IntersectionPoint(message1, message2, message3, message4);
+
+
+
+
+
+
